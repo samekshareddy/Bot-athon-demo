4660b0339de312d17ef3ed716793448cdb08eb2e
Sameksha Reddy <samekshareddy@Samekshas-MacBook-Air.local>
2016-12-10 15:24:02 +0530

    webhooks added

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
 console.log("please work")
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'Bot-athon-westerosairways') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
