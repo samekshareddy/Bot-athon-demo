1e21fb992bc3387e2813c905b0d00e377ad24ebe
Sameksha Reddy <samekshareddy@Samekshas-MacBook-Air.local>
2016-12-11 00:32:49 +0530

    Adding Flight plan extended

--- index.js
+++ index.js
@@ -219,10 +219,10 @@ app.post('/webhook', function (req, res) {
 
       }*/
 
-      else
+      /*else
       {
       	sendMessage(event.sender.id,{text: 'Sorry I did not understand'});
-      }
+      }*/
     res.sendStatus(200);
 });
 
