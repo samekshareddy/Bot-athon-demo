73bca798db1abe364409ffdf7f535368e72f7220
Sameksha Reddy <samekshareddy@Samekshas-MacBook-Air.local>
2016-12-10 15:09:03 +0530

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+console.log("please work")
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
